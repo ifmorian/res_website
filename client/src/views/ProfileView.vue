5d141ce17d12d078a1c3e7f0f3fcab84817d5176
ifmorian <felix.kurz2010@gmail.com>
2023-08-14 12:01:05 +0200

    profile/playstyle

--- client/src/views/ProfileView.vue
+++ client/src/views/ProfileView.vue
@@ -5,6 +5,8 @@
       <div class="card-border"></div>
       <div class="card">
         <div class="card-title" ref="cardTitle">
+          <!-- <video src="../assets/animations/trophycelebration_loop.webm" autoplay loop muted class="card-title-background"></video> -->
+          <video src="../assets/animations/summoner-object-magic-action-blue-idle.webm" autoplay loop muted class="card-title-background-idle"></video>
           <div class="pfp-wrapper">
             <div class="pfp-border"></div>
             <div class="pfp">
@@ -15,10 +17,16 @@
             <div class="card-title-tag">{{ accountInfo.gamertag }}</div>
             <div class="card-title-name">@{{ accountInfo.username }}</div>
           </div>
+          <div class="card-title-position">
+            <div class="card-title-position-icon-wrapper">
+              <img src="../assets/images/lol/roles/jungle.png" alt="" class="card-title-position-icon">
+            </div>
+            <div class="card-title-position-name">Icarus</div>
+          </div>
         </div>
         <div class="content">
-          <div class="bio">{{ accountInfo.bio }}</div>
-          <div class="roles" ref="roles">
+          <div class="bio section">{{ accountInfo.bio }}</div>
+          <div class="roles section" ref="roles">
             <div class="roles-circle" ref="rolesCircle">
               <span>c</span>
               <span>l</span>
@@ -47,7 +55,27 @@
                 <img src="../assets/images/lol/roles/support.png" alt="">
               </div>
             </div>
-            <div class="roles-title">Main Roles</div>
+            <div class="roles-title title">Main Roles</div>
+          </div>
+          <div class="style section">
+            <div class="title">Playstyle</div>
+            <div class="style-sliders">
+              <div class="slider">
+                <div class="slider-range left">Safe</div>
+                <div class="slider-range right">Agressive</div>
+                <div class="slider-bar" ref="slideBar1">
+                  <div class="slider-bar-value" :style="{left: playstyle + '%', background: playstyleColor}">
+                    <svg class="top" viewBox="0 0 2 2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                      <polygon points=".6,.5 1,1 1.4,.5" :style="{stroke: playstyleColor}"></polygon>
+                    </svg>
+                    <svg class="bottom" viewBox="0 0 2 2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                      <polygon points=".6,.5 1,1 1.4,.5" :style="{stroke: playstyleColor}"></polygon>
+                    </svg>
+                    <div class="slider-bar-value-interact" ref="playstyleInteract"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -73,6 +101,8 @@
         mainChamps: new Array(),
         accountInfo: Object(),
         role: 0,
+        playstyle: 20,
+        playstyleColor: 'rgb(255,0,0)'
       }
     },
     beforeCreate() {
@@ -125,6 +155,36 @@
       roleImage.addEventListener('mouseleave', () => {
         rotate(-80, 10);
       });
+
+      const updateColor = () => {
+        this.playstyleColor = 'rgb('+ (112 + this.playstyle/100 * 143) + ',' + (228 - this.playstyle/100 * 228) + ',' + (112 - this.playstyle/100 * 112) + ')';
+      }
+      const updatePlaystyle = (value: number) => {
+        this.playstyle = 5 + value * (9/10);
+      }
+      updatePlaystyle(this.playstyle)
+      updateColor();
+    
+      let clicked1 = false;
+      let bar1 = this.$refs.slideBar1 as HTMLDivElement;
+      let interact1 = this.$refs.playstyleInteract as HTMLDivElement;
+      interact1.addEventListener('mousedown', () => {
+        clicked1 = true;
+        document.body.style.cursor = 'grabbing';
+      });
+      document.addEventListener('mouseup', () => {
+        clicked1 = false
+        document.body.style.cursor = 'unset';
+      });
+      document.addEventListener('mousemove', (e: MouseEvent) => {
+        if (!clicked1) return;
+        let bounds = bar1.getBoundingClientRect();
+        let x = Math.round((e.clientX - bounds.left) / bounds.width * 10) * 10;
+        if (x < 0) x = 0;
+        else if (x > 100) x = 100;
+        updatePlaystyle(x)
+        updateColor();
+      })
     }
   }
 </script>
@@ -176,7 +236,6 @@
   .card-wrapper {
     max-width: 700px;
     width: 90%;
-    height: 1000px;
     opacity: .8;
     margin-bottom: 10%;
   }
@@ -186,8 +245,8 @@
     left: -1vw; top: -1vw;
     width: calc(100% + 2vw);
     height: calc(100% + 2vw);
-    background: radial-gradient(#7e116c, #47879b, #7e116c);
-    border-radius: 1.2vw;
+    background: radial-gradient(#7e116c, var(--secondary), #7e116c);
+    border-radius: 1.25vw;
     border: .35vw solid #7e116c73;
     background-size: 200% 200%;
     background-position: 0% 0%;
@@ -201,11 +260,11 @@
   }
 
   .card {
-    background: linear-gradient(to bottom right, rgba(0, 82, 76, .6), rgba(109, 206, 178, .6));
-    border: .3vw solid #2a0624;
+    background: var(--color-background-soft);
+    border: .4vw solid #2a0624;
     border-radius: 1vw;
-    height: 100%;
     width: 100%;
+    padding-bottom: 5%;
   }
 
   .pfp-wrapper {
@@ -221,6 +280,7 @@
     justify-content: center;
     overflow: hidden;
     border-radius: 50%;
+    background: var(--color-background-soft)
   }
 
   .pfp > img {
@@ -229,11 +289,12 @@
   }
 
   .pfp-border {
+    opacity: .8;
     position: absolute;
     left: -5%; top: -5%;
-    width: 110%;
     height: 110%;
-    background: linear-gradient(#7e116c, #47879b);
+    aspect-ratio: 1;
+    background: linear-gradient(#7e116c, var(--secondary));
     border-radius: 50%;
     outline: 1px solid rgba(10, 10, 10, .5);
     transform: rotate(0deg);
@@ -257,7 +318,7 @@
     align-items: center;
     position: sticky;
     transition: height .1s, font-size .1s;
-    margin-bottom: 4%;
+    margin-bottom: 7%;
     z-index: 5;
     padding: 0 4%;
     padding-bottom: 2%;
@@ -265,10 +326,32 @@
   }
 
   .card-title.is-pinned {
-    height: 210px;
+    height: 240px;
     font-size: 12px;
     backdrop-filter: blur(100px);
-    border-bottom: .3vw solid #2a0624;
+    border-bottom: .4vw solid #2a0624;
+    overflow: hidden;
+  }
+
+  .card-title-background-idle {
+    position: absolute;
+    width: 100%;
+    left: 0;
+    transform: translateY(-3%);
+    mask: linear-gradient(transparent, white, transparent)
+  }
+
+  .card-title-background {
+    position: absolute;
+    width: 100%;
+    right: 0%;
+    transform: translateY(5%);
+    transition: width .3s, right .3s;
+  }
+
+  .card-title.is-pinned .card-title-background {
+    width: 55%;
+    right: 22.5%;
   }
 
   .card-title-name-wrapper {
@@ -276,13 +359,21 @@
     flex-direction: column;
     justify-content: center;
     margin-left: 3%;
+    width: 320px;
+  }
+
+  .card-title.is-pinned .card-title-name-wrapper {
+    width: 400px;
   }
 
   .card-title-tag {
     color: var(--color-heading);
     font-family: 'Austere';
-    font-size: 3em;
+    font-size: 1.8em;
     line-height: 130%;
+    text-overflow: '...';
+    white-space: nowrap;
+    overflow: hidden;
   }
 
   .card-title-name {
@@ -292,10 +383,45 @@
     font-family: monospace;
   }
 
+  .card-title-position {
+    width: 50%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+
+  .card-title-position-icon-wrapper {
+    height: 50%;
+  }
+
+  .card-title-position-icon {
+    height: 100%;
+  }
+
+  .card-title-position-name {
+    color: var(--color-heading);
+    font-size: 200%;
+    font-weight: bold;
+    margin-left: 2%;
+    text-shadow: black 0px 0px 10px;
+  }
+
   .content {
     width: 100%;
     height: 100%;
     padding: 0 4%;
+  }
+
+  .content .section {
+    margin-bottom: 10%;
+  }
+
+  .content .title {
+    color: var(--color-heading);
+    font-size: 200%;
+    margin-bottom: 10%;
   }
 
   .bio {
@@ -309,7 +435,6 @@
   .roles {
     width: 100%;
     left: 0%;
-    margin-top: 8%;
     transition: width .2s;
     display: flex;
     flex-direction: row;
@@ -340,14 +465,13 @@
     text-align: center;
     font-size: 100%;
     font-weight: bold;
-    transition: transform .2s;
+    transition: transform .3s ease-in-out;
   }
 
   .roles-title {
-    font-size: 200%;
     width: 40%;
     margin-left: 3%;
-    color: var(--color-heading);
+    margin-bottom: 0 !important;
   }
 
   .roles.in-sight > .roles-title {
@@ -366,7 +490,6 @@
   .roles.in-sight > img {
     width: 100%;
     border-width: 6px;
-    border-color: var(--color-background-mute)
   }
 
   .roles-logo {
@@ -438,4 +561,93 @@
     left: 56%;
     top: 45%;
   }
+
+  .style-sliders {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .slider {
+    width: 70%;
+  }
+
+  .slider-range {
+    position: absolute;
+    top: -95%;
+    font-size: 130%;
+  }
+
+  .slider-range.right {
+    right: 0;
+  }
+
+  .slider-bar {
+    width: 100%;
+    height: 40px;
+    border: 2px solid black;
+    outline: 3px solid var(--secondary);
+    border-radius: 20px;
+    background: linear-gradient(to right, var(--success), var(--error));
+    filter: drop-shadow(0 0 7px black);
+  }
+
+  .slider-bar::after {
+    content: '';
+    position: absolute;
+    bottom: 0; left: 50%;
+    width: 90%; height: 65%;
+    background-image: linear-gradient(to right, transparent 46%, rgba(10, 10, 10, .5) 46% 54%, transparent 54%);
+    background-size: 10% 10px;
+    transform: translateX(-50%);
+  }
+
+  .slider-bar-value {
+    position: absolute;
+    top: 0;
+    height: 100%;
+    width: 4px;
+    transform: translateX(-50%);
+    filter: drop-shadow(1px 1px 0px black)
+      drop-shadow(-1px 1px 0px black)
+      drop-shadow(1px -1px 0px black)
+      drop-shadow(-1px -1px 0px black);
+    z-index: 2;
+  }
+
+  .slider-bar-value > svg {
+    position: absolute;
+    width: 25px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .slider-bar-value > svg > polygon {
+    stroke-width: .45;
+    stroke-linejoin: round;
+  }
+
+  .slider-bar-value .top {
+    top: -13px;
+  }
+
+  .slider-bar-value .bottom {
+    bottom: -13px;
+    transform: translateX(-50%) scale(1, -1);
+  }
+
+  .slider-bar-value-interact {
+    position: absolute;
+    background: transparent;
+    width: 16px;
+    height: 160%;
+    top: -30%;
+    left: -6px;
+    cursor: grab;
+  }
+
+  .slider-bar-value-interact.grabbed {
+    cursor: grabbing;
+  }
+  
 </style>
